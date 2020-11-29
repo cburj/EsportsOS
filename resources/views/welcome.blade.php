7170ee46e38ade36437ddaf980baf1d9e15227be
cburj <charlieburgess1998@gmail.com>
2020-11-29 12:21:52 +0000

    Just changed the intro page so it isn't as cringe :smile:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-4 font-weight-normal">{{config('app.name', 'FYP')}}</h1>
-      <p class="lead font-weight-normal">EsportsOS is designed to allow all sizes of esports tournaments to be organised in a few simple clicks,
-        with the ability to record match results and rank teams/players based on their performances.
-          EsportsOS also gives admins the ability to create several dashboards that can be used within livestreams/broadcasts to be shared data with viewers and attendees.</p>
+      <p class="lead font-weight-normal">EsportsOS is a template/framework for tournament organisers to build their own custom event management system. 
+        EOS provides a staring point, where players and teams can be created and matches can be automatically generated using a selection of algorithms.
+        There is also support for data-driven dashboards, where information can be presented to viewers directly in livestreams, utilising reusable and responsive components.
+      </p>
+      <p>Note: Most functionality is hidden to non-admin users.</p>
      </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
