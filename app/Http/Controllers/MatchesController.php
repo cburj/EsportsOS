7b79d3548654c18af3f6d0307968c44115b5e75c
cburj <charlieburgess1998@gmail.com>
2020-12-12 20:02:49 +0000

    Get all added back in?

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Team;
 
 class MatchesController extends Controller
 {
@@ -13,7 +14,8 @@ class MatchesController extends Controller
      */
     public function index()
     {
-        //
+        $matches = Match::get();
+        return view('matches.index')->with('matches', $matches);
     }
 
     /**
