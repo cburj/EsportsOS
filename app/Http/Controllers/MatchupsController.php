2142c26ae1e55bf22060ceeeac1e7ce3b4ebce22
cburj <charlieburgess1998@gmail.com>
2021-03-04 10:56:43 +0000

    Fixed message query, now only messages after lastRequest.

--- app/Http/Controllers/MatchupsController.php
+++ app/Http/Controllers/MatchupsController.php
@@ -227,8 +227,10 @@ class MatchupsController extends Controller
     {
         try
         {
-            $timestamp = strtotime($request->last_request);
-            $date_time = date("Y-m-d H:i:s", $timestamp);
+            $date_time = date("Y-m-d H:i:s", $request->last_request);
+
+            //Log::channel('general')->info('>>> DATETIME:' . $date_time . ', JS timestamp: ' . $request->last_request . '');
+
             $messages = DisputeMessage::where('matchup_id', $request->matchup_id)->where('created_at', '>', $date_time)->get();
             return response()->json(array('messages' => $messages), 200);
         }
