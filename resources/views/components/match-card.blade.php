f36d33c64c2a23d917d58960f17d9fa4934bd40e
cburj <charlieburgess1998@gmail.com>
2021-03-26 15:33:03 +0000

    rework to match card to allow for partially planned

--- resources/views/components/match-card.blade.php
+++ resources/views/components/match-card.blade.php
@@ -52,20 +52,95 @@
                         <i class="fas fa-server pr-2"></i>JOIN SERVER
                     </a>
                 </div>
-            @else
-            @if ($verbose == 'true')
-                <div class="matches-index-logos">
-                    <img src='/img/teams/Default.png' class="img-thumbnail float-right" />
-                    <img src='/img/teams/Default.png' class="img-thumbnail float-left" />
+
+
+            @elseif ($matchup->team1_id != null)
+                @if ($verbose == 'true')
+                    <div class="matches-index-logos">
+                        <a href="/teams/{{ $matchup->team1->id }}">
+                            <img src="/img/teams/{{ $matchup->team1->name }}.png" class="img-thumbnail float-left"
+                                onerror="this.onerror=null; this.src='/img/teams/Default.png'" />
+                        </a>
+                        <img src='/img/teams/Default.png' class="img-thumbnail float-right" />
+                    </div>
+                @endif
+                <div class="text-center">
+                    <h3>UNCONFIRMED*</h3>
+
+                    @php
+                        $dateString = $matchup->date_time;
+                        $date = new DateTime($dateString);
+                        $cur_date = new DateTime();
+                        $endString = $matchup->end_time;
+                        $end_date = new DateTime($endString);
+                    @endphp
+
+                    @if ($cur_date <= $date)
+                        <p>Starts at: {{ $date->format('d/m/Y @ H:i') }}</p>
+                    @elseif($dateString == null)
+                        <p>Unscheduled (TBD)</p>
+                    @endif
+
+                    <p><strong>{{ $matchup->team1->name }}</strong></p>
+                    <p>VS</p>
+                    <p>{{ $matchup->child2->team1->name }} or {{ $matchup->child2->team2->name }}</p>
                 </div>
-            @endif
+
+
+
+                @elseif ($matchup->team2_id != null)
+                @if ($verbose == 'true')
+                    <div class="matches-index-logos">
+                        <img src='/img/teams/Default.png' class="img-thumbnail float-left" />
+                        <a href="/teams/{{ $matchup->team2->id }}">
+                            <img src="/img/teams/{{ $matchup->team2->name }}.png" class="img-thumbnail float-right"
+                                onerror="this.onerror=null; this.src='/img/teams/Default.png'" />
+                        </a>
+                    </div>
+                @endif
+                <div class="text-center">
+                    <h3>UNCONFIRMED*</h3>
+
+                    @php
+                        $dateString = $matchup->date_time;
+                        $date = new DateTime($dateString);
+                        $cur_date = new DateTime();
+                        $endString = $matchup->end_time;
+                        $end_date = new DateTime($endString);
+                    @endphp
+
+                    @if ($cur_date <= $date)
+                        <p>Starts at: {{ $date->format('d/m/Y @ H:i') }}</p>
+                    @elseif($dateString == null)
+                        <p>Unscheduled (TBD)</p>
+                    @endif
+
+                    <p>{{ $matchup->child1->team1->name }} or {{ $matchup->child1->team2->name }}</p>
+                    <p>VS</p>
+                    <p><strong>{{ $matchup->team2->name }}</strong></p>
+                </div>
+
+
+
+
+
+
+            @else
+                @if ($verbose == 'true')
+                    <div class="matches-index-logos">
+                        <img src='/img/teams/Default.png' class="img-thumbnail float-right" />
+                        <img src='/img/teams/Default.png' class="img-thumbnail float-left" />
+                    </div>
+                @endif
                 <div class="text-center">
                     <h3>UNCONFIRMED*</h3>
                     @if ($matchup->child1->team1 != null && $matchup->child1->team2 != null && $matchup->child2->team1 != null && $matchup->child2->team2 != null)
-                        <p><strong>{{ $matchup->child1->team1->name }} or {{ $matchup->child1->team2->name }}</strong>
+                        <p><strong>{{ $matchup->child1->team1->name }} or
+                                {{ $matchup->child1->team2->name }}</strong>
                         </p>
                         <p>VS</p>
-                        <p><strong>{{ $matchup->child2->team1->name }} or {{ $matchup->child2->team2->name }}</strong>
+                        <p><strong>{{ $matchup->child2->team1->name }} or
+                                {{ $matchup->child2->team2->name }}</strong>
                         </p>
                     @else
                         <p><strong>Winner of <a href="/matchups/{{ $matchup->child1->id }}">Match
@@ -81,16 +156,17 @@
                         $endString = $matchup->end_time;
                         $end_date = new DateTime($endString);
                     @endphp
-                    
-                    @if($dateString == null)
+
+                    @if ($dateString == null)
                         <p>Unscheduled (TBD)</p>
                     @else
-                    <p>Starts at: {{ $date->format('d/m/Y @ H:i') }}</p>
+                        <p>Starts at: {{ $date->format('d/m/Y @ H:i') }}</p>
                     @endif
 
                     @if ($verbose == 'true')
-                    <small>*The participants of this match are determined by the outcomes of matches which haven't yet
-                        been played.</small>
+                        <small>*The participants of this match are determined by the outcomes of matches which haven't
+                            yet
+                            been played.</small>
                     @endif
                 </div>
             @endif
