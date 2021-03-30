a03ff7604d165d9b2fe042fed495f9b28c478b1b
cburj <charlieburgess1998@gmail.com>
2021-03-30 11:49:58 +0100

    Welcome screen update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         	<p>The Esports Organisation System is designed to hell small-scale Esports events get up and running, without the need to develop their own software.</p>
         	<p>The application is designed to be as user-friendly as possible, with just a few clicks you can input all of your players & teams and generate a full event schedule.</p>
 
-        	<a type="button" class="btn btn-danger btn-rounded mx-0" href="/matches">View Matches</a>
+        	<a type="button" class="btn btn-danger btn-rounded mx-0" href="/matchups">View Matches</a>
 
         </div>
       </div>
@@ -49,55 +49,62 @@
 
 
   <div class="container my-5">
-    <!--Section: Content-->
+
+    <!-- Section -->
     <section>
   
-      <h6 class="font-weight-normal text-uppercase font-small grey-text mb-4 text-center">FAQ</h6>
-      <!-- Section heading -->
-      <h3 class="font-weight-bold black-text mb-4 pb-2 text-center">Frequently Asked Questions</h3>
-      <hr class="w-header">
-      <!-- Section description -->
-      <p class="lead text-muted mx-auto mt-4 pt-2 mb-5 text-center">Got a question? We've got answers!</p>
+      <h6 class="font-weight-bold text-center grey-text text-uppercase small mb-4">Features</h6>
+      <h3 class="font-weight-bold text-center dark-grey-text pb-2">More About our Application</h3>
+      <hr class="w-header my-4">
+      <p class="lead text-center text-muted pt-2 mb-5"></p>
   
       <div class="row">
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">What is EsportOS</h5>
-          <p class="text-muted">EsportsOS is the best place to host your small-scale Esports event. You can easily setup your event in a few clicks, without the need for any third party applications.</p>
-        </div>
-
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">Is it secure?</h5>
-          <p class="text-muted">You bet it is! EOS has built in authentication and privelleges, so you can resitrict access and keep tabs on malicious behaviour. The platform is also 100% yours - you don't share databases or resources with other events!</p>
+  
+        <div class="col-md-6 col-xl-3 mb-4">
+          <div class="card text-center bg-danger text-white">
+            <div class="card-body">
+              <p class="mt-4 pt-2"><i class="fas fa-file-code fa-4x"></i> <i class="fas fa-cloud fa-4x"></i></p>
+              <h5 class="font-weight-normal my-4 py-2"><a class="text-white" href="#">Built using Web Technologies</a></h5>
+              <p class="mb-4">By developing a web-based application, you can deploy it in the cloud or on a local server - giving you complete control over who can access the system.</p>
+            </div>
+          </div>
         </div>
   
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">Who is EsportsOS for?</h5>
-          <p class="text-muted">The application is designed for event organisers of Esports events. We recommend the application for numbers of teams of between 4-32</p>
+        <div class="col-md-6 col-xl-3 mb-4">
+          <div class="card text-center">
+            <div class="card-body">
+              <p class="mt-4 pt-2"><i class="fas fa-mobile-alt fa-4x grey-text"></i> <i class="fas fa-desktop fa-4x grey-text"></i></p>
+              <h5 class="font-weight-normal my-4 py-2"><a class="dark-grey-text" href="#">Cross Platform</a></h5>
+              <p class="text-muted mb-4">There's no need for users to install apps or software onto their devices, just open the application in their browser - even on a mobile device!</p>
+            </div>
+          </div>
         </div>
   
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">How can I setup the application?</h5>
-          <p class="text-muted">We understand that not all Esports events will have/require an internet connection, so EsportOS can be hosted locally or in the cloud - it's completely up to you!</p>
+        <div class="col-md-6 col-xl-3 mb-4">
+          <div class="card text-center deep-purple lighten-1 text-white">
+            <div class="card-body">
+              <p class="mt-4 pt-2"><i class="fas fa-shield-alt fa-4x"></i></p>
+              <h5 class="font-weight-normal my-4 py-2"><a class="text-white" href="#">Secure By Design</a></h5>
+              <p class="mb-4">EsportsOS comes with authentication and logging out of the box. So you can protect your data and identify risks.</p>
+            </div>
+          </div>
         </div>
   
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">What about streaming my event?</h5>
-          <p class="text-muted">EsportsOS comes built in with several responsive "Stream Assets" that can be used to output data to viewers of your livestreams, or even used as infographics for event attendees.</p>
+        <div class="col-md-6 col-xl-3">
+          <div class="card text-center">
+            <div class="card-body">
+              <p class="mt-4 pt-2"><i class="fas fa-users fa-4x grey-text"></i></i></p>
+              <h5 class="font-weight-normal my-4 py-2"><a class="dark-grey-text" href="#">User-Friendly</a></h5>
+              <p class="text-muted mb-4">Participants can easily login to their account, submit scores and keep up to date on their event schedule.</p>
+            </div>
+          </div>
         </div>
   
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">What about event moderation?</h5>
-          <p class="text-muted">Admin users can quickly identify issues using our built in logging system, and talk directly to players to resolve any match conflicts.</p>
-        </div>
-  
-        <div class="col-md-6 col-lg-4 mb-4">
-          <h5 class="font-weight-normal mb-3">What do players have access to?</h5>
-          <p class="text-muted">Players are at the heart of the application. As a player, you have access to your full match schedule, historical match data and personal profiles.</p>
-        </div>
       </div>
   
     </section>
-    
+    <!-- Section -->
+  
   </div>
 
 
